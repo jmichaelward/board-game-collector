9acac8e876a376c6e00d67edae220679d7e91a55
Jeremy Ward <jeremy@3five.com>
2017-02-26 13:57:19 -0600

    Fix indentation in markup of settings view.

--- views/settings.php
+++ views/settings.php
@@ -8,7 +8,7 @@
 ?>
 
 <form method="POST" action="options.php">
-<?php settings_fields( 'bgg-settings' ); ?>
+	<?php settings_fields( 'bgg-settings' ); ?>
 	<?php do_settings_sections( 'bgg-settings' ); ?>
 	<?php submit_button(); ?>
 </form>
