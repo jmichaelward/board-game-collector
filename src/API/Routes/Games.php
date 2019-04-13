3d13a5ba6d846a03928375f64417d640ece36e1f
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-04-13 12:44:16 -0500

    Clean up formatting in Games route.

--- src/API/Routes/Games.php
+++ src/API/Routes/Games.php
@@ -13,10 +13,14 @@ class Games {
 	 * Register data fields to return in the standard Games response.
 	 */
 	public function register_api_fields() {
-		register_rest_field( 'bgc_game', 'metadata', [
-			'get_callback' => function ( $post ) {
-				return get_post_meta( $post['id'], 'bgc_game_meta' );
-			},
-		] );
+		register_rest_field(
+			'bgc_game',
+			'metadata',
+			[
+				'get_callback' => function ( $post ) {
+					return get_post_meta( $post['id'], 'bgc_game_meta' );
+				},
+			]
+		);
 	}
 }
