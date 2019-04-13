0a7f1d5a44e909c88dbc668c9aba191b8a74404b
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-04-13 11:38:45 -0500

    Add Auryn injector to CliService to assist with command object creation.

--- src/Cli/CliService.php
+++ src/Cli/CliService.php
@@ -1,6 +1,7 @@
 <?php
 namespace JMichaelWard\BoardGameCollector\Cli;
 
+use Auryn\Injector;
 use JMichaelWard\BoardGameCollector\Cli\Command\BgcCommand;
 use WebDevStudios\OopsWP\Structure\Service;
 
@@ -10,6 +11,26 @@ use WebDevStudios\OopsWP\Structure\Service;
  * @package JMichaelWard\BoardGameCollector\Service
  */
 class CliService extends Service {
+	/**
+	 * Auryn\Injector instance.
+	 *
+	 * @var Injector
+	 * @since 2019-04-13
+	 */
+	private $injector;
+
+	/**
+	 * CliService constructor.
+	 *
+	 * @param Injector $injector Auryn\Injector instance.
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-04-13
+	 */
+	public function __construct( Injector $injector ) {
+		$this->injector = $injector;
+	}
+
 	/**
 	 * Set of Cli commands registered to this plugin.
 	 *
@@ -34,7 +55,7 @@ class CliService extends Service {
 	public function register_commands() {
 		foreach ( $this->commands as $command_name => $command_class ) {
 			try {
-				\WP_CLI::add_command( $command_name, $command_class );
+				\WP_CLI::add_command( $command_name, $this->injector->make( $command_class ) );
 			} catch ( \Exception $e ) {
 				\WP_CLI::error( $e->getMessage() );
 			}
