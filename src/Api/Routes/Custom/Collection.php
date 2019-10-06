08f9f58e84e2570fa46dcaa139298755e6451421
Jeremy Ward <jeremy.ward@webdevstudios.com>
2019-10-06 16:40:24 -0500

    Update Collection route to account for POST request to update collection.

--- src/Api/Routes/Custom/Collection.php
+++ src/Api/Routes/Custom/Collection.php
@@ -9,8 +9,12 @@
 
 namespace JMichaelWard\BoardGameCollector\Api\Routes\Custom;
 
+use JMichaelWard\BoardGameCollector\Admin\Settings\SettingsFields;
 use JMichaelWard\BoardGameCollector\Api\BoardGameGeek;
 use JMichaelWard\BoardGameCollector\Api\Routes\CustomRestRoute;
+use JMichaelWard\BoardGameCollector\Model\Games\BGGGame;
+use JMichaelWard\BoardGameCollector\Model\Games\BGGGameAdapter;
+use JMichaelWard\BoardGameCollector\Updater\GamesUpdater;
 
 /**
  * Class Collection
@@ -37,28 +41,41 @@ class Collection extends CustomRestRoute {
 	public function register_routes() {
 		register_rest_route(
 			$this->namespace,
-			"/{$this->rest_base}",
+			$this->rest_base,
 			[
 				'methods'             => \WP_REST_Server::CREATABLE,
 				'callback'            => [ $this, 'update_items' ],
-				'permission_callback' => [ $this, 'verify_nonce' ],
+				'args'                => [
+					'username' => [
+						'required'          => true,
+						'validate_callback' => [ $this, 'validate_username' ],
+					],
+				],
+				'permission_callback' => function ( \WP_REST_Request $request ) {
+					return wp_verify_nonce( $request->get_param( 'nonce' ), 'wp_rest' );
+				},
 			]
 		);
 	}
 
 	/**
-	 * @TODO Add nonce verification.
-	 *
-	 * @param \WP_REST_Request $request The WordPress REST request.
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-10-04
+	 * @return string
+	 */
+	private function get_saved_username() {
+		return get_option( SettingsFields::SETTINGS_KEY )[ SettingsFields::USERNAME_KEY ] ?? '';
+	}
+
+	/**
+	 * @param $username
 	 *
 	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
-	 * @since  2019-09-01
+	 * @since  2019-10-04
 	 * @return bool
 	 */
-	public function verify_nonce( \WP_REST_Request $request ) {
-		$nonce = $request->get_param( 'wp_nonce' );
-
-		return true;
+	public function validate_username( $username ) {
+		return  ! empty( $username ) && $username === $this->get_saved_username();
 	}
 
 	/**
@@ -66,15 +83,58 @@ class Collection extends CustomRestRoute {
 	 *
 	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
 	 * @since  2019-09-01
-	 * @return void
 	 */
 	public function update_items() {
-		$api = new BoardGameGeek();
+		$unprocessed = get_transient( BoardGameGeek::COLLECTION_TRANSIENT_KEY );
 
-		delete_transient( BoardGameGeek::COLLECTION_TRANSIENT_KEY );
+		if ( ! $unprocessed ) {
+			$api = new BoardGameGeek();
 
-		$request = $api->get_collection( get_option( 'bgc-settings' )[ 'bgg-username' ] );
+			$collection = $api->get_collection( get_option( SettingsFields::SETTINGS_KEY )[ SettingsFields::USERNAME_KEY ] );
 
-		return $request;
+			if ( isset( $collection['status'] ) && 202 === $collection['status'] ) {
+				return new \WP_REST_Response( [ 'status' => 202 ], 202 );
+			}
+
+			set_transient( BoardGameGeek::COLLECTION_TRANSIENT_KEY, $collection, 5 * MINUTE_IN_SECONDS );
+
+			return $collection;
+		}
+
+		return $this->process_remaining_games( $unprocessed );
+	}
+
+	/**
+	 * @param $games
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-10-04
+	 */
+	private function process_remaining_games( $games ) {
+		$updater          = new GamesUpdater();
+		$games_to_process = $this->get_games_to_process( $games );
+
+		foreach ( $games_to_process as $index => $game ) {
+			if ( $updater->save_game_data( $game ) ) {
+				unset( $games[ $index ] );
+			}
+		}
+
+		$games = array_values( $games );
+
+		set_transient( BoardGameGeek::COLLECTION_TRANSIENT_KEY, $games, 5 * MINUTE_IN_SECONDS );
+
+		return $games;
+	}
+
+	/**
+	 * @param array $games
+	 *
+	 * @author Jeremy Ward <jeremy.ward@webdevstudios.com>
+	 * @since  2019-10-04
+	 * @return array
+	 */
+	private function get_games_to_process( array $games ) {
+		return array_splice( $games, 0, ( count( $games ) <= 10 ?: 10 ) );
 	}
 }
