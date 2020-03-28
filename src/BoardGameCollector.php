a47420e5f3674d090e7d28c34129798c27584c66
Jeremy Ward <jeremy@jmichaelward.com>
2020-03-28 16:34:42 -0500

    Refactor service initialization process in main plugin class to allow service sharing.

--- src/BoardGameCollector.php
+++ src/BoardGameCollector.php
@@ -3,12 +3,14 @@ namespace JMichaelWard\BoardGameCollector;
 
 use Auryn\ConfigException;
 use Auryn\Injector;
+use JMichaelWard\BoardGameCollector\Model\Hydratable;
 use WebDevStudios\OopsWP\Structure\Plugin\Plugin;
 use JMichaelWard\BoardGameCollector\Content\ContentRegistrar;
 use JMichaelWard\BoardGameCollector\Api\ApiService;
 use JMichaelWard\BoardGameCollector\UI\Cli\CliService;
 use JMichaelWard\BoardGameCollector\Cron\CronService;
 use JMichaelWard\BoardGameCollector\Admin\Settings;
+use WebDevStudios\OopsWP\Structure\Service;
 
 /**
  * Class BoardGameCollector
@@ -17,12 +19,25 @@ use JMichaelWard\BoardGameCollector\Admin\Settings;
  */
 final class BoardGameCollector extends Plugin {
 	/**
-	 * The plugin bootstrap file.
+	 * Array of instantiated Service objects.
 	 *
-	 * @var string
-	 * @since 2019-09-02
+	 * IMPORTANT: The order of these services matter as of 3/28/20.
+	 * ************************************************************
+	 * The CliService now has a dependency on the Settings service. In order for Auryn to properly share the
+	 * same object with the CliService, it's necessary for us to initialize that class first, and hydrate it
+	 * with data from the WordPress settings panel. This will ensure that values such as a person's username
+	 * for BoardGameGeek will get included in API requests.
+	 *
+	 * @see BoardGameCollector::init_services()
+	 * @var array
 	 */
-	private $plugin_file;
+	protected $services = [
+		Settings::class,
+		ContentRegistrar::class,
+		ApiService::class,
+		CliService::class,
+		CronService::class,
+	];
 
 	/**
 	 * Auryn\Injector instance.
@@ -31,19 +46,6 @@ final class BoardGameCollector extends Plugin {
 	 * @since 2019-04-12
 	 */
 	private $injector;
-
-	/**
-	 * Array of instantiated Service objects.
-	 *
-	 * @var array
-	 */
-	protected $services = [
-		ContentRegistrar::class,
-		ApiService::class,
-		CliService::class,
-		CronService::class,
-		Settings::class,
-	];
 
 	/**
 	 * BoardGameCollector constructor.
@@ -81,15 +83,64 @@ final class BoardGameCollector extends Plugin {
 	 */
 	protected function init_services() {
 		$objects = array_map(
-			function ( $object_classname ) {
+			function ( $service_classname ) {
+				$service = $this->injector->make( $service_classname );
+
+				if ( $this->is_hydratable_service( $service_classname ) ) {
+					$service->hydrate();
+				}
+
+				if ( $this->is_shared_service( $service_classname ) ) {
+					$this->share_service( $service );
+				}
+
 				return [
-					'namespace' => $object_classname,
-					'object'    => $this->injector->make( $object_classname ),
+					'namespace' => $service_classname,
+					'object'    => $service,
 				];
 			},
 			$this->services
 		);
 
 		return array_column( $objects, 'object', 'namespace' );
+	}
+
+	/**
+	 * Sets up the service for sharing.
+	 *
+	 * @param Service $service Service instance.
+	 *
+	 * @throws ConfigException If there's a problem finding the Service within Auryn.
+	 */
+	private function share_service( Service $service ) {
+		$this->injector->share( $service );
+	}
+
+	/**
+	 * Check whether the service is shared.
+	 *
+	 * @param string $service_classname The fully-qualified class name of the Service.
+	 *
+	 * @return bool
+	 */
+	private function is_shared_service( $service_classname ) {
+		return in_array(
+			$service_classname,
+			[
+				Settings::class,
+			],
+			true
+		);
+	}
+
+	/**
+	 * Check whether the Service requires hydration before use.
+	 *
+	 * @param string $service_classname The fully-qualified class name of the Service.
+	 *
+	 * @return bool
+	 */
+	private function is_hydratable_service( $service_classname ) {
+		return in_array( Hydratable::class, class_implements( $service_classname ), true );
 	}
 }
