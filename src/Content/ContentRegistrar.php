7899f694141ebe712dcd1a3ce5cd5e9057b1f6e0
Jeremy Ward <jeremy@jmichaelward.com>
2021-11-27 09:58:28 -0600

    Add return types.

--- src/Content/ContentRegistrar.php
+++ src/Content/ContentRegistrar.php
@@ -61,7 +61,7 @@ class ContentRegistrar extends Service {
 	 * @author Jeremy Ward <jeremy@jmichaelward.com>
 	 * @since  2019-04-12
 	 */
-	public function register_hooks() {
+	public function register_hooks(): void {
 		add_action( 'init', [ $this, 'register_post_types' ] );
 		add_action( 'init', [ $this, 'register_taxonomies' ] );
 		add_action( 'init', [ $this, 'register_editor_blocks' ] );
@@ -74,7 +74,7 @@ class ContentRegistrar extends Service {
 	 * @author Jeremy Ward <jeremy@jmichaelward.com>
 	 * @since  2019-04-12
 	 */
-	public function register_post_types() {
+	public function register_post_types(): void {
 		foreach ( $this->post_types as $post_type_class ) {
 			$post_type = new $post_type_class();
 			$this->register_content_type( $post_type );
@@ -87,7 +87,7 @@ class ContentRegistrar extends Service {
 	 * @author Jeremy Ward <jeremy@jmichaelward.com>
 	 * @since  2019-04-12
 	 */
-	public function register_taxonomies() {
+	public function register_taxonomies(): void {
 		foreach ( $this->taxonomies as $taxonomy_class ) {
 			$taxonomy = new $taxonomy_class();
 			$this->register_content_type( $taxonomy );
@@ -103,7 +103,7 @@ class ContentRegistrar extends Service {
 	 * @since  2019-12-14
 	 * @return void
 	 */
-	public function register_editor_blocks() {
+	public function register_editor_blocks(): void {
 		foreach ( $this->editor_blocks as $editor_block_class ) {
 			$editor_block = new $editor_block_class();
 			$editor_block->set_file_path( $this->file_path );
@@ -117,7 +117,7 @@ class ContentRegistrar extends Service {
 	 * @author Jeremy Ward <jeremy@jmichaelward.com>
 	 * @since  2019-04-12
 	 */
-	public function add_featured_image_support() {
+	public function add_featured_image_support(): void {
 		add_theme_support( 'post-thumbnails' );
 	}
 
@@ -130,7 +130,7 @@ class ContentRegistrar extends Service {
 	 * @since  2019-04-12
 	 * @return void
 	 */
-	private function register_content_type( ContentTypeInterface $content_type ) {
+	private function register_content_type( ContentTypeInterface $content_type ): void {
 		$content_type->register();
 	}
 }
